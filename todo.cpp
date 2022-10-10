4c01f2348518a14689b4043a5d84f8ea128f8ebc
jpaz03 <97417198+jpaz03@users.noreply.github.com>
2022-10-09 17:41:43 -0700

    Update todo.cpp

--- todo.cpp
+++ todo.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main() {
+int main(int argc, char* argv[])
+{
   std::cout << "todo: make a todo app..." << '\n';
 }
